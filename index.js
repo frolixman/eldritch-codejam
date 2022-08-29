229c001cfb42360feab358b34783ce095c92d1bd
frolixman <frolovpo@gmail.com>
2022-08-29 16:40:29 +0300

    feat: make a page

---
+++ index.js
@@ -0,0 +1,17 @@
+import ancients from "./assets/Ancients/index.js";
+// import blue_cards from "./assets/MythicCards/blue/index.js";
+// import brown_cards from "./assets/MythicCards/brown/index.js";
+// import green_cards from "./assets/MythicCards/green/index.js";
+
+// const card = document.querySelector('.top-card');
+// card.addEventListener('click', () => {
+//     card.classList.add('temp-card');
+// });
+console.log(ancients);
+
+// console.log(blue_cards);
+// console.log(brown_cards);
+// console.log(green_cards);
+
+
+
